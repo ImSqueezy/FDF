0c458ded2f9c508a02855c4235a468ff89e617f0
ImSqueezy <ouaallaabdelali1@gmail.com>
2025-01-21 17:22:54 +0100

    new changes

--- main.c
+++ main.c
@@ -1,31 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aouaalla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 16:08:26 by aouaalla          #+#    #+#             */
+/*   Updated: 2025/01/06 16:08:27 by aouaalla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "FDF.h"
 
-int	check_valid(const char *str)
+int	connection_init(char *map, t_gl *gl_ptr)
 {
-	int		i;
-	char	*base;
+	char	*title;
 
-	base = "-0123456789abcdef";
-	i = 0;
-	if (*base)
+	title = ft_strjoin("aouaalla's FDF : ", map);
+	gl_ptr->mlx_ptr = mlx_init();
+	if (!gl_ptr->mlx_ptr)
 		return (0);
-	if (!ft_strncmp(base, str, ft_strlen(str)))
-		return (1);
+	gl_ptr->win_ptr = mlx_new_window(gl_ptr->mlx_ptr, SIZE_X, SIZE_Y, title);
+	if (!gl_ptr->win_ptr)
+		return (0);
+	gl_ptr->img.mlx_img = mlx_new_image(gl_ptr->mlx_ptr, SIZE_X, SIZE_Y);
+	if (!gl_ptr->img.mlx_img)
+		return (0);
+	gl_ptr->img.addr = mlx_get_data_addr(gl_ptr->img.mlx_img,
+			&gl_ptr->img.bp_pixel, &gl_ptr->img.line_len, &gl_ptr->img.endian);
+	if (!gl_ptr->img.addr)
+		return (0);
+	if (SIZE_X / gl_ptr->width/2 < SIZE_Y / gl_ptr->height/2)
+	{
+		gl_ptr->zoom = SIZE_X / gl_ptr->width/2;
+		if (gl_ptr->zoom > 50)
+			gl_ptr->zoom = 1;
+	}
+	else
+		gl_ptr->zoom = SIZE_Y / gl_ptr->height/2;
 	return (0);
 }
 
-int	ft_atoi_base(const char *str, int str_base)
+
+int	draw(t_gl *gl_ptr)
 {
-	if (check_valid(str))
-		str_base = 0;
+	int	i;
+	int	j;
+
+	if (!gl_ptr->win_ptr)
+		return (0);
+	i = -1;
+	while (++i < gl_ptr->height)
+	{
+		j = -1;
+		while (++j < gl_ptr->width)
+		{
+			if (j != gl_ptr->width - 1)
+				line_draw((t_coor){j, i, (j + 1), i, gl_ptr->map[i][j].color},
+				gl_ptr);
+			if (i != gl_ptr->height - 1)
+				line_draw((t_coor){j, i, j, (i + 1), gl_ptr->map[i][j].color},
+				gl_ptr);
+		}
+	}
+	mlx_put_image_to_window(gl_ptr->mlx_ptr, gl_ptr->win_ptr,
+		gl_ptr->img.mlx_img, 0, 0);
 	return (0);
 }
 
+int	key_handle(int keysysm, t_gl *gl_ptr)
+{
+	if (keysysm == XK_Escape)
+	{
+		mlx_destroy_window(gl_ptr->mlx_ptr, gl_ptr->win_ptr);
+		gl_ptr->win_ptr = NULL;
+		printf("WINDOW CLOSED!");
+	}
+	// printf("%d\n", keysysm);
+	return (0);
+}
+
+void	hooks(t_gl *gl_ptr)
+{
+	mlx_hook(gl_ptr->win_ptr, KeyPress, KeyPressMask, &key_handle, gl_ptr);
+}
 
 int main(int argc, char **argv)
 {
-	if (argc != 3)
-		return(printf("invalid number of args!"), 0);
-	printf("%d", ft_atoi_base(argv[1], ft_atoi(argv[2])));
+	int		fd;
+	t_gl	gl;
+
+	if (argc != 2)
+		return (write(2, "error: Invalid number of args", 28), 1);
+	fd = open (argv[1], O_RDONLY);
+	if (fd < 0)
+		return (perror("open"), 1);
+	if (file_check(argv[1], fd, &gl))
+		return (0);
+	connection_init(argv[1], &gl);
+	draw(&gl);
+	mlx_loop_hook(gl.mlx_ptr, &key_handle, &gl);
+	hooks(&gl);
+	mlx_loop(gl.mlx_ptr);
+	mlx_destroy_image(gl.mlx_ptr, gl.img.mlx_img);
+    mlx_destroy_display(gl.mlx_ptr);
 	return (0);
 }
--- parsing.c
+++ parsing.c
@@ -12,30 +12,46 @@
 
 #include "FDF.h"
 
-static void	get_map_data(char *filename, t_gl *data_ptr)
+static int	get_map_width(int fd, t_gl *gl_ptr)
 {
 	int		i;
-	int		fd;
+	int		j;
 	char	*tmp;
 
-	data_ptr->width = 0;
-	data_ptr->height = 0;
-	fd = open(filename, O_RDONLY);
+	gl_ptr->map_width = malloc(gl_ptr->height * sizeof(t_map));
+	if (!gl_ptr->map_width)
+		return (write(2, "error: malloc failure!", 22), 1);
+	j = -1;
 	tmp = get_next_line(fd);
-	i = 0;
-	while (tmp[i])
-	{
-		if ((tmp[i] != 32 && tmp[i + 1] == 32) || tmp[i + 1] == '\0')
-			(*data_ptr).width++;
-		i++;
-	}
 	while (tmp)
 	{
+		gl_ptr->map_width[++j].width = 0;
+		i = -1;
+		while (tmp[++i])
+		{
+			if ((tmp[i] != ' ' && tmp[i] != '\n') && (tmp[i + 1] == '\n'
+					|| tmp[i + 1] == ' ' || tmp[i + 1] == '\0'))
+				gl_ptr->map_width[j].width++;
+		}
 		free(tmp);
 		tmp = get_next_line(fd);
-		(*data_ptr).height++;
 	}
-	close(fd);
+	close (fd);
+	return (0);
+}
+
+static int	check_valid(t_gl *gl_ptr)
+{
+	int	i;
+
+	i = 0;
+	while (i < gl_ptr->height)
+	{
+		if (gl_ptr->map_width[0].width != gl_ptr->map_width[i].width)
+			return (write(2, "error: invalid map!", 19), 1);
+		i++;
+	}
+	return (0);
 }
 
 static void	fill_matrix(char *line, t_map *arr, t_gl *data_ptr)
@@ -47,8 +63,8 @@ static void	fill_matrix(char *line, t_map *arr, t_gl *data_ptr)
 	line_splitted = ft_split(line, ' ');
 	if (!line_splitted)
 		return ;
-	i = 0;
-	while (i < (*data_ptr).width)
+	i = -1;
+	while (++i < (*data_ptr).width)
 	{
 		arr[i].z = ft_atoi(line_splitted[i]);
 		if (ft_strchr(line_splitted[i], ','))
@@ -57,41 +73,64 @@ static void	fill_matrix(char *line, t_map *arr, t_gl *data_ptr)
 			while (line_splitted[i][j] && line_splitted[i][j] != ',')
 				j++;
 			arr[i].color = ft_atoi_base(&line_splitted[i][j + 1],
-					"0123456789ABCDEF");
+					"0123456789abcdef");
 		}
 		else
 			arr[i].color = 0x00ff00;
 		free(line_splitted[i]);
-		i++;
 	}
 	free(line_splitted);
 }
 
-int	file_check(char	*file, t_gl *data_ptr)
+int	get_map_data(char *filename, t_gl *data_ptr)
+{
+	int		fd;
+	char	*tmp;
+
+	data_ptr->height = 0;
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (perror("open"), 1);
+	tmp = get_next_line(fd);
+	while (tmp)
+	{
+		free(tmp);
+		tmp = get_next_line(fd);
+		(*data_ptr).height++;
+	}
+	close(fd);
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (perror("open"), 1);
+	if (get_map_width(fd, data_ptr) || check_valid(data_ptr))
+		return (1);
+	return (0);
+}
+
+int	file_check(char	*file, int fd, t_gl *data_ptr)
 {
 	int		i;
-	int		fd;
 	char	*line;
 
-	fd = open(file, O_RDONLY);
 	if (fd < 0)
-		return (perror("open"), 0);
-	get_map_data(file, data_ptr);
+		return (perror("open"), 1);
+	if (get_map_data(file, data_ptr))
+		return (1);
+	data_ptr->width = data_ptr->map_width[1].width;
 	(*data_ptr).map = malloc((*data_ptr).height * sizeof(t_map *));
 	if (!(*data_ptr).map)
-		return (write(2, "error: malloc failure!", 22), 0);
-	i = 0;
-	while (i < (*data_ptr).height)
+		return (write(2, "error: malloc failure!", 22), 1);
+	i = -1;
+	while (++i < (*data_ptr).height)
 	{
 		line = get_next_line(fd);
 		if (!line)
-			return (0);
+			return (1);
 		(*data_ptr).map[i] = malloc((*data_ptr).width * sizeof(t_map));
 		if (!(*data_ptr).map)
-			return (write(2, "error: malloc failure!", 22), 0);
+			return (write(2, "error: malloc failure!", 22), 1);
 		fill_matrix(line, (*data_ptr).map[i], data_ptr);
-		i++;
 		free(line);
 	}
-	return (1);
+	return (0);
 }
