79ef90d4ec7cdca57513a35af87dc8d22ddcd5a3
ImSqueezy <ouaallaabdelali1@gmail.com>
2025-01-29 20:29:02 +0100

    started the bonus (user interacting, hooks)
    so far, handled the colors for high low and base altitude points, still have some problems to fix, left some comments in main.c, and the project isn't organized yet

--- fdf_linux/main.c
+++ fdf_linux/main.c
@@ -38,16 +38,85 @@ int program_exit(t_gl *gl_ptr)
 {
 	mlx_destroy_image(gl_ptr->mlx_ptr, gl_ptr->img.mlx_img);
 	mlx_destroy_window(gl_ptr->mlx_ptr, gl_ptr->win_ptr);
+	gl_ptr->img.addr == NULL;
 	gl_ptr->win_ptr = NULL;
+	gl_ptr->mlx_ptr = NULL;
 	return (0);
+}
+
+int	zoom_in_out(int code, t_gl *data)
+{
+	if (code == 45)
+		data->zoom /= 1.24;
+	if (data->zoom >= 1345.080933)
+		return (0);
+	if (code == 61)
+	{
+		if (data->zoom == 0)
+			data->zoom += 1.24;
+		data->zoom *=1.24;
+	}
+	printf("%f\n", data->zoom);
+	return (0);
+}
+
+void z_manipulator(int *point, int *color, int code, t_gl *data)
+{
+	if (code == 107 && *point != 0)
+	{
+		if (*point != -1 && *point != 0)
+			*point += 1;
+		else
+			*point += 2;
+	}
+	else if (code == 106 && *point != 0)
+	{
+		if (*point != 1)
+			*point -= 1;
+		else
+			*point -= 2;
+	}
+	if (data->colored == 0)
+	{
+		if (*point < 0)
+			*color = data->mc.low_altitude_color;
+		else if (*point >= 0)
+			*color = data->mc.high_altitude_color;
+	}
+}
+
+void	change_altitude(int code, t_gl *data)
+{
+	int i;
+	int	j;
+
+	i = -1;
+	while (++i < data->height)
+	{
+		j = -1;
+		while (++j < data->width)
+		{
+			// if (code == 110 && data->map[i][j].z == 0)
+			// 	data->map[i][j].color = 0xff0000;
+			if ((code == 106 || code == 107) && data->map[i][j].z != 0)
+				z_manipulator(&data->map[i][j].z, &data->map[i][j].color, code, data);
+			else if (code == 110 && data->map[i][j].z == 0)
+				data->map[i][j].color = BLACK;
+		}
+	}
 }
 
 int	key_handle(int keysysm, t_gl *gl_ptr)
 {
-	if (keysysm == 53)
+	if (keysysm == XK_Escape)
 		program_exit(gl_ptr);
-	else
-		printf("%d\n", keysysm);
+	if (keysysm == 61 || keysysm == 45)
+		zoom_in_out(keysysm, gl_ptr);
+	if (keysysm == 107 || keysysm == 106 || 110) // bug needs to be fixed:
+									// the big map julia altitude changing translates the whole graph
+									// even the point that has the altitude of 0
+		change_altitude(keysysm, gl_ptr);
+	printf("%d\n", keysysm);
 	return (0);
 }
 
@@ -55,13 +124,15 @@ int main(int argc, char **argv)
 {
 	t_gl	gl;
 
-	if (argc != 2)
+	if (argc < 1)
 		return (printf("args!"), 0);
 	file_check(argv[1], &gl);
 	connection_init(argv[1], &gl);
 	mlx_loop_hook(gl.mlx_ptr, draw, &gl);
 	mlx_hook(gl.win_ptr, 17, 0, &program_exit, &gl);
-	mlx_key_hook(gl.win_ptr, &key_handle, &gl);
+	mlx_hook(gl.win_ptr, KeyPress, KeyPressMask, &key_handle, &gl);
+	// mlx_key_hook(gl.win_ptr, &key_handle, &gl);
 	mlx_loop(gl.mlx_ptr);
+	// free the map
 	return (0);
 }
